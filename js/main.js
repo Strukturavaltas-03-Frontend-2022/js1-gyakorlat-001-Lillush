fcec07c03b50e3480903e55de8ebe54f9be54a75
Lillush <lillush88@gmail.com+>
2022-08-09 20:17:42 +0200

    megvan!

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 
 const handleClick = () => {
   let buttons = document.querySelectorAll(".button");
-        for (let i = 0; 1 < buttons.length; i+=1) {
+        for (let i = 0; i < buttons.length; i+=1) {
         buttons[i].addEventListener('click', function() {
         console.log(buttons[i].innerHTML);
        })
